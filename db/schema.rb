26233cb6a64ae18a707699b7ff5bfc0e76e20a6e
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-06 20:20:22 +0530

    Add message model,and add validations and associations to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_142538) do
+ActiveRecord::Schema.define(version: 2020_06_06_143952) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"

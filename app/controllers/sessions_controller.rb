3543edbf0de5d4c3c874c80d0b348a838a80e37f
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-06 22:46:22 +0530

    add restrictions to the logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redeirect, only: [:new, :create]
   def new
   end
 
@@ -18,5 +19,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil 
     flash[:success] = 'You are successfully logged out'
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redeirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 end
